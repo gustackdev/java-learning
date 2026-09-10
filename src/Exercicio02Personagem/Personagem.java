e0a79a993265a77a7a828068fc09a40cb4a0fadf
Gustavo S. Andrade <guspatty17@gmail.com>
2026-09-09 23:58:02 -0300

    feat: add attributes and abilities to Personagem class

--- src/Exercicio02Personagem/Personagem.java
+++ src/Exercicio02Personagem/Personagem.java
@@ -1,4 +1,74 @@
 package Exercicio02Personagem;
 
-public class Personagem {
+public class Personagem
+{
+    String nome;
+    int hp;
+    int mana;
+    double exp;
+    int nivel;
+    int custoQ;
+    int custoW;
+    int custoE;
+    int custoR;
+
+    void habilidadeQ()
+    {
+        if (mana >= custoQ)
+        {
+            mana -= custoQ;
+
+            System.out.println("Você usou a habilidade Q.");
+            System.out.println("Mana atual: "+ mana);
+        }
+        else
+        {
+            System.out.println("Sem mana o suficiente.");
+        }
+    }
+
+    void habilidadeW()
+    {
+        if (mana >= custoW)
+        {
+            mana -= custoW;
+
+            System.out.println("Você usou a habilidade W.");
+            System.out.println("Mana atual: "+ mana);
+        }
+        else
+        {
+            System.out.println("Sem mana o suficiente.");
+        }
+    }
+
+    void habilidadeE()
+    {
+        if (mana >= custoE)
+        {
+            mana -= custoE;
+
+            System.out.println("Você usou a habilidade E.");
+            System.out.println("Mana atual: "+ mana);
+        }
+        else
+        {
+            System.out.println("Sem mana o suficiente.");
+        }
+    }
+
+    void habilidadeR()
+    {
+        if (mana >= custoR)
+        {
+            mana -= custoR;
+
+            System.out.println("Você usou a habilidade R.");
+            System.out.println("Mana atual: "+ mana);
+        }
+        else
+        {
+            System.out.println("Sem mana o suficiente.");
+        }
+    }
 }
